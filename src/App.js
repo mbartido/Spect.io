d00851f8f2f673294275071326e771d052668d2b
Michael Bartido <mbartido@ucsc.edu>
2019-02-28 01:17:41 -0800

    More landing page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
-import logo from './photos/logo.svg';
+import Header from './components/Header';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p className="App-logo-header">Spect.io</p>
-        </header>
+        <Header />
+        <p className="info">A music visualizer for Spotify.</p>
+        <p className="landingLogin">Sign up or Login to get started.</p>
+        <a href="#" className="greenButton">Get Started</a>
       </div>
     );
   }
