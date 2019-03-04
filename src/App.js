a366f33845072cf7bb5b2c3fd7f9f92d7fb7ff63
Michael Bartido <mbartido@ucsc.edu>
2019-03-03 16:55:57 -0800

    Added image to landing page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
+import wave from './photos/wave.png';
 import './App.css';
 
 class App extends Component {
@@ -9,6 +10,7 @@ class App extends Component {
         <Header />
         <p className="info">A music visualizer for Spotify.</p>
         <p className="landingLogin">Sign up or Login to get started.</p>
+        <img src={wave} id="wave" alt="landingWave" />
         <a href="#" className="greenButton">Get Started</a>
       </div>
     );
